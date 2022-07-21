122f4a79dff69d7f1b72c571c38f371a7494acd3
Dzmitry Harashchenia <qwester345@ya.ru>
2022-07-21 21:34:40 +0300

    some in user

--- users/src/main/java/ya/qwester345/users/dao/entity/enums/Role.java
+++ users/src/main/java/ya/qwester345/users/dao/entity/enums/Role.java
@@ -1,9 +1,16 @@
 package ya.qwester345.users.dao.entity.enums;
 
-public enum Role {
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
     ROLE_ADMIN,
     ROLE_USER;
 
     Role() {
+    }
+
+    @Override
+    public String getAuthority() {
+        return this.name();
     }
 }
